04dc3b7c7cfaf63c723990d3d500eceb2011be08
minihu-crypto <46985966+minihu2020@users.noreply.github.com>
2022-07-06 19:50:14 +0800

    init

--- leetcode/1.cpp
+++ leetcode/1.cpp
@@ -1,2 +1,18 @@
+#include <algorithm>
+#include <cstdio>
+#include <functional>
+#include <iostream>
+#include <string>
 #include <vector>
 using namespace std;
+
+int main(int argc, char* argv[]) {
+    string programName(argv[0]);
+    string in_str = programName + "In.txt", out_str = programName+"Out.txt";
+    const char* IN_FILE = in_str.c_str();
+    const char* OUT_FILE = out_str.c_str();
+    freopen(IN_FILE, "r", stdin);
+    freopen(OUT_FILE, "w", stdout);
+    
+    return 0;
+}
