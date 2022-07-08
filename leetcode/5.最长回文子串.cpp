9c5f0b8dcb35a428869d4dfbddddbe5403cc7402
minihu-crypto <503411681@qq.com>
2022-07-08 10:58:51 +0800

    day4

--- leetcode/5.最长回文子串.cpp
+++ leetcode/5.最长回文子串.cpp
@@ -30,8 +30,7 @@ class Solution {
         for (int i = 0; i < size; ++i) {
             int left1, right1;
             std::tie(left1, right1) = expandAroundCentet(s, i, i);
-            int left2, right2;
-            tie(left2, right2) = expandAroundCentet(s, i, i + 1);
+            auto [left2, right2] = expandAroundCentet(s, i, i + 1);
             if (right1 - left1 > end - start) {
                 start = left1;
                 end = right1;
@@ -83,7 +82,7 @@ class Solution {
 
 int main() {
     Solution solution;
-    cout << solution.longestPalindrome("aacabdkacaa") << endl;
+    cout << solution.longestPalindrome_dp("aacabdkacaa") << endl;
     return 0;
 }
 // @lc code=end
